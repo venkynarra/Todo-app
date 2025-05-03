b5d9df919a161cc5cbc113a18ee5c659d23d2fed
venkatesh <venkateshnarra368@gmail.com>
2025-05-03 12:42:07 -0500

    chnaging themes, add clock , handling edit and complete error's by try and except(IndexError)

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import functions
 import FreeSimpleGUI as sg
-
+import time
+sg.theme("DarkPurple4")
+clock = sg.Text('', key = "clock" )
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip= "Enter todo", key = "todo") # key istodo here
 add_button = sg.Button("ADD") # adding button
@@ -11,13 +13,15 @@ complete_button = sg.Button("complete")
 exit_button = sg.Button("exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label],
+                   layout=[[clock],
+                       [label],
                            [input_box, add_button]
                        ,[list_box, edit_button, complete_button],
                         [exit_button]],
                   font=('helvetica', 20)) # layout is a argument, it expect lists. these lists contain FreeSimpleGUI instances
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout = 200)
+    window['clock'].update(value = time.strftime("%b %d, %Y, %H:%M:%S"))
     print(1, event)  # event gets the values of add from line 6
     print(2, values)  # values get the key from line 5 and input text from same line
     print(3, values['todos'])
@@ -29,20 +33,28 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos) #using this to print adding values in GUI.
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] #todo is key here getting new todo to edit function
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit) #all thse 4 lines of code is replacing new todo with existing todo, this is edit function
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos) # this will give list box instace ,
+            try:
+
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] #todo is key here getting new todo to edit function
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit) #all thse 4 lines of code is replacing new todo with existing todo, this is edit function
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos) # this will give list box instace ,
+            except IndexError:
+                sg.popup("please select an item first", font = ("Helvetica", 20))
         case "complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos) # updating the list and wiritng new todos
-            window['todos'].update(values = todos)
-            window['todo'].update(value = '') # making sure there is no value in todo in entering the input
+            try:
+
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos) # updating the list and wiritng new todos
+                window['todos'].update(values = todos)
+                window['todo'].update(value = '') # making sure there is no value in todo in entering the input
+            except IndexError:
+                sg.popup("please select an item first", font = ("Helvetica", 20))
         case "exit":
             break
         case 'todos':
