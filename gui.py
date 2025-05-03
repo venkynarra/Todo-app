b84b08fa2a0ac8da268c0157c9b62e484b413a9e
venkatesh <venkateshnarra368@gmail.com>
2025-05-03 12:14:44 -0500

    Implememted Complete and exit functions in the GUI.

--- gui.py
+++ gui.py
@@ -7,8 +7,14 @@ add_button = sg.Button("ADD") # adding button
 list_box = sg.Listbox(values = functions.get_todos(), key = "todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("complete")
+exit_button = sg.Button("exit")
+
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button]
+                       ,[list_box, edit_button, complete_button],
+                        [exit_button]],
                   font=('helvetica', 20)) # layout is a argument, it expect lists. these lists contain FreeSimpleGUI instances
 while True:
     event, values = window.read()
@@ -21,7 +27,7 @@ while True:
             new_todo = values['todo'] + "\n"  #we are giving values of the key which we given on top
             todos.append(new_todo)
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            window['todos'].update(values=todos) #using this to print adding values in GUI.
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo'] #todo is key here getting new todo to edit function
@@ -30,10 +36,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos) # this will give list box instace ,
+        case "complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos) # updating the list and wiritng new todos
+            window['todos'].update(values = todos)
+            window['todo'].update(value = '') # making sure there is no value in todo in entering the input
+        case "exit":
+            break
         case 'todos':
             window['todo'].update(value = values['todos'][0])
         case sg.WIN_CLOSED:
             break
-
-
 window.close()
