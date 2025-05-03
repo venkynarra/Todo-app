58721cb15c0c49b26f4928ab4274e3c3777dcad0
venkatesh <venkateshnarra368@gmail.com>
2025-05-03 16:55:12 -0500

    updating thee new todo to todos.txt and deleting whe user selected the checklist

--- web.py
+++ web.py
@@ -9,7 +9,14 @@ st.title("My Todo App")
 st.subheader("this is my todo app")
 st.write("this app is to increase your productivity")
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f"todo_{index}")
+    checkbox = st.checkbox(todo, key=f"{todo}-{index}")
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+          # deleting the todo from session state
+        st.rerun()
+
 
 st.text_input(label= "", placeholder= "add new todo",
               on_change=add_todo, key='new_todo')
+
